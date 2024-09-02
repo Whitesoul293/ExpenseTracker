6f509504753c5ee936693c4335e441ec306486a0
Whitesoul293 <157641062+Whitesoul293@users.noreply.github.com>
2024-09-02 21:30:21 +0200

    Impaginazione app

---
+++ script.js
@@ -0,0 +1,4 @@
+const inputText = document.getElementById('inputText');
+const inputPrice = document.getElementById('inputPrice');
+const inputDate = document.getElementById('inputDate');
+const myBtn = document.getElementById('myBtn');
